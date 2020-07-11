c07a589d998d1f5d5319d2faddbe935174ccbfd8
sunakan <sunakan03@gmail.com>
2020-07-11 08:20:44 +0000

    lambda

--- common-lisp/lambda.lisp
+++ common-lisp/lambda.lisp
@@ -1,3 +1,4 @@
-(setq list1 '(2 3 5 7 11 13))
+(print '-------mapでlambdaを渡してそれぞれの要素に対して計算)
 (print
-  (mapcar (lambda (n) (* n n)) list1))
+  (let ((list1 '(1 2 3 4 5 6)))
+        (mapcar (lambda (x) (* x x)) list1)))
